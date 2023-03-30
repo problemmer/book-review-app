b9b6c33497d76638aeca1d967d3982de7ca9474a
dellaao <dellaoctaamelia@gmail.com>
2023-03-30 17:25:29 +0700

    comment cl (masih banyak bug)

--- src/component/comment/index.js
+++ src/component/comment/index.js
@@ -1,15 +1,29 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import './styles.css'
+import CommentCl from '../coment-cl'
 
 function Comment(props) {
   return (
     <div className='comments-container'>
       <div className='comments-content'>
-        Comments
+        <p className='comment-title'>Comments</p>
+        <p className='comment-total'>6 komentar</p>
+        <div className='comments'> 
+          <CommentCl/>
+          <CommentCl/>
+          <CommentCl/>
+          <CommentCl/>
+          <CommentCl/>
+          <CommentCl/>
+        </div>
+        <div className='button-comment-container'>
+          <button className='button-comment'>
+              Comment
+          </button>
+        </div>
+        
       </div>
-        
-        
     </div>
   )
 }
